01f331ae24cdf1a1e7839395c5fa20b1d8398fa7
0trebor0 <git config --global user.email>
2023-08-18 14:58:04 +0100

    first release

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # DOMSculptor
+
+let div = create('div',document.body);
+
+let video = div.createChild('video');
+video.setAttribute('autoplay');
+video.setAttribute('controls');
+video.setAttribute('src','https://example/example.mp4');
+video.setStyle('width','100%');
+video.setStyle('height','100%');
+
+div return {
+    html DOM,
+    addClass(value)=>{…},
+    appendChild( child )=>{…},
+    children[{…}],
+    containsClass( value )=>{…},
+    getAttribute(name)=> {…},
+    hasAttribute(name)=> {…},
+    hide()=> {…},
+    off( event, callback )=> {…},
+    on( event, callback )=> {…},
+    removeAttribute(name)=> {…},
+    removeClass(value)=> {…},
+    setAttribute(name,value)=> {…},
+    setStyle( property, value)=> {…},
+    setText( text )=> {…}
+}
---
+++ src/index.js
@@ -0,0 +1,132 @@
+/**
+ * function to  create dom element
+ * @param {name} string - the name of the element
+ * @param {parent} object - the dom, object of element
+ * @param {callback} function -the callback function
+ * @returns {object} - returns {}
+ */
+export default create = ( name,parent=null,callback=null ) =>{
+
+    let e = {};
+    
+    e.html = document.createElement( name );
+    
+    e.children = [];
+
+    e.setAttribute = (name,value='')=>{
+
+        return e.html.setAttribute(name, value);
+
+    }
+
+    e.removeAttribute = (name)=>{
+
+        return e.html.removeAttribute(name);
+
+    }
+
+    e.getAttribute = (name)=>{
+
+        return e.html.getAttribute(name);
+
+    }
+
+    e.hasAttribute = (name)=>{
+
+        return e.html.hasAttribute(name);
+
+    }
+
+    e.addClass = (value)=>{
+
+        return e.html.classList.add(value);
+
+    }
+
+    e.removeClass = (value)=>{
+
+        return e.html.classList.remove(value);
+
+    }
+
+    e.containsClass = (value)=>{
+
+        return e.html.classList.contains(value);
+
+    }
+
+    e.setText = ( text )=>{
+
+        e.html.textContent = text;
+    
+    }
+
+    e.setStyle = ( property, value)=>{
+    
+        e.html.style[property] = value;
+    
+    }
+
+    e.hide = ()=>{
+    
+        return e.setStyle( 'display', 'none' );
+    
+    }
+
+    e.appendChild = ( child )=>{
+    
+        return e.html.appendChild( child );
+    
+    }
+
+    e.createChild = ( name,callback=null )=>{
+
+        if( typeof callback !== 'function' ){
+            callback = null;
+        }
+    
+        return create( name,e,callback );
+    
+    }
+
+    e.on = ( event, callback )=>{
+    
+        return e.html.addEventListener(event, callback);
+    
+    }
+
+    e.off = ( event, callback )=>{
+    
+        return e.html.removeEventListener(event, callback);
+    
+    }
+
+    if( parent == null ){
+
+        document.body.lastChild.appendChild( e.html );
+
+    }else if( 'html' in parent ){
+
+        parent.children.push( e );
+
+        parent.html.appendChild( e.html );
+
+    } else if( 'nodeType' in parent && parent.nodeType === 1 ){
+
+        parent.appendChild( e.html );
+
+    } else if( document.querySelector( parent ) ){
+
+        document.querySelector( parent ).appendChild( e.html );
+
+    }
+
+    if( typeof callback == 'function' ){
+
+        callback(e);
+
+    }
+
+    return e;
+
+}
