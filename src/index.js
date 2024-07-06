13aec8123151827d6a2dc3c8cb331a4c3118a409
robert <robertsantos98@hotmail.co.uk>
2024-07-06 13:24:17 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,93 +8,33 @@ class App {
 
         e.children = [];
 
-        e.setAttribute = (name, value = '') => {
+        e.setAttribute = (name, value = '') => e.html.setAttribute(name, value);
 
-            return e.html.setAttribute(name, value);
+        e.removeAttribute = (name) => e.html.removeAttribute(name);
 
-        }
+        e.getAttribute = (name) => e.html.getAttribute(name);
 
-        e.removeAttribute = (name) => {
+        e.hasAttribute = (name) => e.html.hasAttribute(name);
 
-            return e.html.removeAttribute(name);
+        e.addClass = (value) => e.html.classList.add(value);
 
-        }
+        e.removeClass = (value) => e.html.classList.remove(value);
 
-        e.getAttribute = (name) => {
+        e.containsClass = (value) => e.html.classList.contains(value);
 
-            return e.html.getAttribute(name);
+        e.setText = (text) => { e.html.textContent = text; }
 
-        }
+        e.setStyle = (property, value) => { e.html.style[property] = value; }
 
-        e.hasAttribute = (name) => {
+        e.hide = () => e.setStyle('display', 'none');
 
-            return e.html.hasAttribute(name);
+        e.appendChild = (child) => e.html.appendChild(child);
 
-        }
+        e.createChild = (name, callback = null) => this.create(name, e, callback);
 
-        e.addClass = (value) => {
+        e.on = (event, callback) => e.html.addEventListener(event, callback);
 
-            return e.html.classList.add(value);
-
-        }
-
-        e.removeClass = (value) => {
-
-            return e.html.classList.remove(value);
-
-        }
-
-        e.containsClass = (value) => {
-
-            return e.html.classList.contains(value);
-
-        }
-
-        e.setText = (text) => {
-
-            e.html.textContent = text;
-
-        }
-
-        e.setStyle = (property, value) => {
-
-            e.html.style[property] = value;
-
-        }
-
-        e.hide = () => {
-
-            return e.setStyle('display', 'none');
-
-        }
-
-        e.appendChild = (child) => {
-
-            return e.html.appendChild(child);
-
-        }
-
-        e.createChild = (name, callback = null) => {
-
-            if (typeof callback !== 'function') {
-                callback = null;
-            }
-
-            return this.create(name, e, callback);
-
-        }
-
-        e.on = (event, callback) => {
-
-            return e.html.addEventListener(event, callback);
-
-        }
-
-        e.off = (event, callback) => {
-
-            return e.html.removeEventListener(event, callback);
-
-        }
+        e.off = (event, callback) => e.html.removeEventListener(event, callback);
 
         if (parent == null) {
 
@@ -125,5 +65,49 @@ class App {
         return e;
 
     }
-
+    jsontohtml( object ){
+        if( typeof object !== 'object' ){
+            throw new Error('Parameter Not valid JSON Object');
+            return;
+        }
+        if( !("type" in object) ){
+            throw new Error('Must Specify Element Type ');
+            return;
+        }
+        if( !("parent" in object) ){
+            throw new Error('Must Specify The Parent Element ');
+            return;
+        }
+        let element = this.create( object.type, object.parent );
+        if( object.attributes && typeof object.attributes == 'object' ){
+            object.attributes.forEach(attr => element.setAttribute(attr.type, attr.content));
+        }
+        if( object.class && Array.isArray( object.class) ){
+            object.class.forEach(cls => element.addClass(cls));
+        }
+        if( object.onclick && typeof object.onclick == 'function' ){
+            element.on( 'click', object.onclick );
+        }
+        if( ("oncreate" in object && typeof object.oncreate == 'function') ){
+            object.oncreate(element);
+        }
+        if( object.content ){
+            if(typeof object.content == 'string' ){
+                element.setText(object.content);
+            } else if(Array.isArray(object.content)) {
+                object.content.forEach( item=>{
+                    if( typeof item == 'object' ){
+                        item.parent = element;
+                        this.jsontohtml( item );
+                    } else if( typeof item == 'string' ){
+                        element.setText( item );
+                    }
+                } );
+            }
+        }
+        return element;
+    }
+    setTitle( title = '' ){
+        document.title = title;
+    }
 }
