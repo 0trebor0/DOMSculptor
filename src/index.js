33afdecc8a66023c4fbacf7cde18204e80236d2b
0trebor0 <webdevme@outlook.com>
2026-05-05 18:46:04 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,210 +1,170 @@
-class DomSculptor {
+class DomElement {
+    constructor(tagName, sculptor) {
+        this.html = document.createElement(tagName);
+        this.children = [];
+        this._listeners = {};
 
-    // The create method is an instance method of the DomSculptor class
-    create(name, parent = null, callback = null) {
+        const el = this;
 
-        // Create the core object that will hold the element and methods
-        let ele = {
-            html:document.createElement(name),
-            children:[],
-            _listeners:{},
-            attribute:{
-                set: (name, value = '') => {
-                    if (typeof name === 'object' && name !== null) {
-                        for (let key in name) {
-                            if (Object.hasOwnProperty.call(name, key)) {
-                                ele.html.setAttribute(key, name[key]);
-                            }
-                        }
-                    } else if (typeof name === 'string') {
-                        ele.html.setAttribute(name, value);
-                    } else {
-                        console.warn('DomSculptor: attribute.set received invalid name type.', name);
+        this.attribute = {
+            set(name, value = '') {
+                if (typeof name === 'object' && name !== null) {
+                    for (const key in name) {
+                        if (Object.hasOwnProperty.call(name, key)) el.html.setAttribute(key, name[key]);
                     }
-                    return ele;
-                },
-                remove: (name) => {
-                    ele.html.removeAttribute(name);
-                    return ele;
-                },
-                get: (name) => {
-                    return ele.html.getAttribute(name);
-                },
-                has: (name) => {
-                    return ele.html.hasAttribute(name);
-                }
-            },
-            setText:(text)=>{
-                ele.html.textContent = text;
-                return ele;
-            },
-            setStyle:(property, value) => {
-                if (typeof property === 'string' && value !== undefined) {
-                    ele.html.style[property] = value;
+                } else if (typeof name === 'string') {
+                    el.html.setAttribute(name, value);
                 } else {
-                    console.warn('DomSculptor: setStyle received invalid arguments.', property, value);
+                    console.warn('DomSculptor: attribute.set received invalid name type.', name);
                 }
-                return ele;
-           },
-           hide:() =>{
-                ele.setStyle('display', 'none');
-           },
-           show:()=>{
-                ele.setStyle('display', '')
-           },
-           child:{
-                append: (child) => {
-                    if (child && child.html && child.html instanceof Node) {
-                        ele.html.appendChild(child.html);
-                        ele.children.push(child);
-                    } else if (child instanceof Node) {
-                        ele.html.appendChild(child);
-                    } else if (typeof child === 'string') {
-                        ele.html.appendChild(document.createTextNode(child));
-                    } else {
-                        console.warn('DomSculptor: child.append received invalid child type.', child);
-                    }
-                    return ele;
-                },
-                create: (name, callbackOrOptions = null) => {
-                    let childEle = this.create(name, ele, callbackOrOptions);
-                    return childEle;
-                },
-                remove: () => { ele.remove(); }
+                return el;
             },
-            class:{
-                add: (...values) => {
-                    if (values.length > 0) { ele.html.classList.add(...values); }
-                    return ele;
-                },
-                remove: (...values) => {
-                     if (values.length > 0) { ele.html.classList.remove(...values); }
-                    return ele;
-                },
-                contains: (value) => {
-                    return ele.html.classList.contains(value);
-                }
-            },
-            on:(event, callback) => {
-                if (typeof event === 'object' && event !== null) {
-                     for (let key in event) {
-                          if (Object.hasOwnProperty.call(event, key) && typeof event[key] === 'function') {
-                              ele.on(key, event[key]);
-                          }
-                     }
-                     return ele;
-                 } else if (typeof event === 'string' && typeof callback === 'function') {
-                     ele.html.addEventListener(event, callback);
-                     if (!ele._listeners[event]) { ele._listeners[event] = []; }
-                     ele._listeners[event].push(callback);
-                 } else {
-                      console.warn(`DomSculptor: Invalid arguments for .on('${event}', ${callback})`);
-                 }
-                 return ele;
-            },
-            off:(event, callback = null) => {
-                if (!ele._listeners[event]) {
-                   if (callback) { ele.html.removeEventListener(event, callback); }
-                   return ele;
-                }
-                if (callback) {
-                    ele.html.removeEventListener(event, callback);
-                    ele._listeners[event] = ele._listeners[event].filter(cb => cb !== callback);
-                    if (ele._listeners[event].length === 0) { delete ele._listeners[event]; }
+            remove(name) { el.html.removeAttribute(name); return el; },
+            get(name) { return el.html.getAttribute(name); },
+            has(name) { return el.html.hasAttribute(name); }
+        };
+
+        this.class = {
+            add(...values) { if (values.length) el.html.classList.add(...values); return el; },
+            remove(...values) { if (values.length) el.html.classList.remove(...values); return el; },
+            contains(value) { return el.html.classList.contains(value); }
+        };
+
+        this.child = {
+            append(child) {
+                if (child instanceof DomElement) {
+                    el.html.appendChild(child.html);
+                    el.children.push(child);
+                } else if (child instanceof Node) {
+                    el.html.appendChild(child);
+                } else if (typeof child === 'string') {
+                    el.html.appendChild(document.createTextNode(child));
                 } else {
-                    ele._listeners[event].forEach(cb => { ele.html.removeEventListener(event, cb); });
-                    delete ele._listeners[event];
+                    console.warn('DomSculptor: child.append received invalid child type.', child);
                 }
-               return ele;
+                return el;
             },
-            remove:() => {
-                for (let eventType in ele._listeners) {
-                     if (Object.hasOwnProperty.call(ele._listeners, eventType)) {
-                          ele._listeners[eventType].forEach(callback => {
-                              ele.html.removeEventListener(eventType, callback);
-                          });
-                     }
-                 }
-                 ele._listeners = {};
-    
-                ele.children.forEach(child => {
-                     if (child && child.remove && typeof child.remove === 'function') { child.remove(); }
-                });
-                ele.children = [];
-    
-                if (ele.html && ele.html.parentNode) {
-                    ele.html.parentNode.removeChild(ele.html);
+            create(name, opts = null) { return sculptor.create(name, el, opts); },
+            remove() { el.remove(); }
+        };
+    }
+
+    setText(text) { this.html.textContent = text; return this; }
+
+    setStyle(property, value) {
+        if (typeof property === 'object' && property !== null) {
+            for (const key in property) {
+                if (Object.hasOwnProperty.call(property, key)) this.html.style[key] = property[key];
+            }
+        } else if (typeof property === 'string' && value !== undefined) {
+            this.html.style[property] = value;
+        } else {
+            console.warn('DomSculptor: setStyle received invalid arguments.', property, value);
+        }
+        return this;
+    }
+
+    hide() { this.html.style.display = 'none'; return this; }
+    show() { this.html.style.display = ''; return this; }
+
+    on(event, callback) {
+        if (typeof event === 'object' && event !== null) {
+            for (const key in event) {
+                if (Object.hasOwnProperty.call(event, key) && typeof event[key] === 'function') {
+                    this.on(key, event[key]);
                 }
-                ele.html = null; // Clean up reference
+            }
+        } else if (typeof event === 'string' && typeof callback === 'function') {
+            this.html.addEventListener(event, callback);
+            if (!this._listeners[event]) this._listeners[event] = [];
+            this._listeners[event].push(callback);
+        } else {
+            console.warn(`DomSculptor: Invalid arguments for .on('${event}', ${callback})`);
+        }
+        return this;
+    }
+
+    off(event, callback = null) {
+        if (!this._listeners[event]) {
+            if (callback) this.html.removeEventListener(event, callback);
+            return this;
+        }
+        if (callback) {
+            this.html.removeEventListener(event, callback);
+            this._listeners[event] = this._listeners[event].filter(cb => cb !== callback);
+            if (!this._listeners[event].length) delete this._listeners[event];
+        } else {
+            this._listeners[event].forEach(cb => this.html.removeEventListener(event, cb));
+            delete this._listeners[event];
+        }
+        return this;
+    }
+
+    remove() {
+        for (const eventType in this._listeners) {
+            if (Object.hasOwnProperty.call(this._listeners, eventType)) {
+                this._listeners[eventType].forEach(cb => this.html.removeEventListener(eventType, cb));
             }
         }
+        this._listeners = {};
+        this.children.forEach(child => { if (child?.remove) child.remove(); });
+        this.children = [];
+        if (this.html?.parentNode) this.html.parentNode.removeChild(this.html);
+        this.html = null;
+    }
+}
 
-        // --- Parent Appending Logic ---
+class DomSculptor {
+    create(tagName, parent = null, callback = null) {
+        const ele = new DomElement(tagName, this);
+
         let parentNode = null;
-
-        if (parent == null) { parentNode = document.body; }
-        else if (parent && typeof parent === 'object' && 'html' in parent && parent.html instanceof Node) {
-             parentNode = parent.html;
-             if (parent.children && Array.isArray(parent.children)) { parent.children.push(ele); } else { parent.children = [ele]; }
+        if (parent == null) {
+            parentNode = document.body;
+        } else if (parent instanceof DomElement) {
+            parentNode = parent.html;
+            parent.children.push(ele);
+        } else if (parent instanceof Node) {
+            parentNode = parent;
+        } else if (typeof parent === 'string') {
+            parentNode = document.querySelector(parent);
+            if (!parentNode) console.warn(`DomSculptor.create: Could not find parent "${parent}". Element not appended.`);
+        } else {
+            console.warn('DomSculptor.create: Invalid parent type. Element not appended.', parent);
         }
-        else if (parent instanceof Node) { parentNode = parent; }
-        else if (typeof parent === 'string') {
-             parentNode = document.querySelector(parent);
-             if (!parentNode) { console.warn(`DomSculptor.create: Could not find parent element with selector: "${parent}". Element not appended.`); }
-        } else { console.warn('DomSculptor.create: Invalid parent type provided. Element not appended.', parent); }
 
-        if (parentNode) { parentNode.appendChild(ele.html); }
-
-        // Execute the optional callback function, passing the created element object
-        if (typeof callback == 'function') { callback(ele); }
+        if (parentNode) parentNode.appendChild(ele.html);
+        if (typeof callback === 'function') callback(ele);
 
         return ele;
     }
+
     jsontohtml(config) {
-        if (typeof config !== 'object' || config === null) {
-            throw new Error('DomSculptor.jsontohtml: Configuration must be a valid object.');
-        }
+        if (typeof config !== 'object' || config === null) throw new Error('DomSculptor.jsontohtml: config must be a valid object.');
         if (!config.type) throw new Error('DomSculptor.jsontohtml: Must specify "type".');
         if (!config.parent) throw new Error('DomSculptor.jsontohtml: Must specify "parent".');
-    
-        // Create the element
-        let element = this.create(config.type, config.parent);
-    
-        // Apply attributes
-        if (config.attributes && typeof config.attributes === 'object') {
-            element.attribute.set(config.attributes);
-        }
-    
-        // Apply classes
-        if (Array.isArray(config.class)) {
-            config.class.forEach(c => element.class.add(c));
-        }
-    
-        // Apply text content
-        if (typeof config.text === 'string') {
-            element.setText(config.text);
-        }
-    
-        // Optional callback
-        if (typeof config.oncreate === 'function') {
-            config.oncreate(element);
-        }
-    
-        // Recursively create children
+
+        const element = this.create(config.type, config.parent);
+
+        if (config.attributes && typeof config.attributes === 'object') element.attribute.set(config.attributes);
+        if (Array.isArray(config.class)) element.class.add(...config.class);
+        if (typeof config.text === 'string') element.setText(config.text);
+        if (typeof config.oncreate === 'function') config.oncreate(element);
+
         if (Array.isArray(config.children)) {
-            config.children.forEach(childConfig => {
-                if (typeof childConfig === 'object' && childConfig !== null) {
-                    this.jsontohtml({ ...childConfig, parent: element });
-                } else if (typeof childConfig === 'string') {
-                    element.child.append(childConfig);
+            config.children.forEach(child => {
+                if (typeof child === 'object' && child !== null) {
+                    this.jsontohtml({ ...child, parent: element });
+                } else if (typeof child === 'string') {
+                    element.child.append(child);
                 } else {
-                    console.warn('DomSculptor.jsontohtml: Invalid child configuration.', childConfig);
+                    console.warn('DomSculptor.jsontohtml: Invalid child configuration.', child);
                 }
             });
         }
-    
+
         return element;
     }
-
-
 }
+
+export default DomSculptor;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'domsculptor.min.js',
     path: path.resolve(__dirname, 'dist'),
+    library: {
+      name: 'DomSculptor',
+      type: 'umd',
+      export: 'default',
+    },
+    globalObject: 'this',
   },
 };
